0fcbba2d9d76d046235e84081f0713c307875a09
aslawter <aslawter@gmail.com>
2014-06-16 17:05:31 -0400

    Add Create and Delete Galleries options

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get "/" => "galleries#index"
+  get "/galleries/new" => "galleries#new"
+  get "/galleries/:id" => "galleries#show"
+  get "/galleries/:id/edit" => "galleries#edit"
+  patch "/galleries/:id" => "galleries#update"
+  delete "/galleries/:id" => "galleries#destroy"
+  post "/galleries/" => "galleries#create"
   end
