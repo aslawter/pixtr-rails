9229d889e7bb6cd368bd97fef61e50b1fffc6c7b
aslawter <aslawter@gmail.com>
2014-06-18 17:54:23 -0400

    Add sign in and user auth

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
    # post "/images" => "images#create"
   end
   resources :users, only: [:create]
+  resource :session, only: [:create]
 
   get "/sign_up", to: "users#new"
+  get "sign_in", to: "sessions#new"
 end
