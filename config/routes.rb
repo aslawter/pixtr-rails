0345a1d351b84731593171cb477771eec8713130
aslawter <aslawter@gmail.com>
2014-06-19 10:47:04 -0400

    Add sign in and sign out feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
-Rails.application.routes.draw do   root to: "galleries#index"
+Rails.application.routes.draw do
+	root to: "galleries#index"
 
   #get to: "galleries#index"
-  resources :galleries, only: [:show, :new, :create, :edit, :update,
-  :destroy]  do
+  resources :galleries, only: [:show, :new, :create, :edit, :update, :destroy]  do
   		resources :images, only: [:new, :create, :edit, :update]
    # get "/images/new" => "images#new"
    # post "/images" => "images#create"
   end
+
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
 end
